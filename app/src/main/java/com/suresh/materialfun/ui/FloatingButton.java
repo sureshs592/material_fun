dfcecfe3968749c10ceb79399f5693bf63f9cf13
Suresh Subramaniam <sureshs.592@gmail.com>
2015-03-21 18:35:15 +0800

    Self sizing (using wrap_content) and applying padding inside the view

--- app/src/main/java/com/suresh/materialfun/ui/FloatingButton.java
+++ app/src/main/java/com/suresh/materialfun/ui/FloatingButton.java
@@ -8,6 +8,8 @@ import android.graphics.Paint;
 import android.graphics.RectF;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.View;
 
 import com.suresh.materialfun.R;
@@ -23,12 +25,15 @@ public class FloatingButton extends View {
     private int btnColor;
     private Drawable btnIcon;
     private RectF circleRect;
+    private int btnSize;
+    private final float BTN_SIZE_DP = 56f;
 
 
     public FloatingButton(Context context, AttributeSet attrs) {
         super(context, attrs);
 
-        TypedArray arr = context.getTheme().obtainStyledAttributes(attrs, R.styleable.FloatingButton, 0, 0);
+        TypedArray arr = context.getTheme().obtainStyledAttributes(
+                attrs, R.styleable.FloatingButton, 0, 0);
 
         try {
             btnColor = arr.getColor(R.styleable.FloatingButton_fb_buttonColor, Color.BLACK);
@@ -38,28 +43,48 @@ public class FloatingButton extends View {
             arr.recycle();
         }
 
-        init();
+        init(context);
     }
 
-    private void init() {
+    private void init(Context context) {
+        //Initialising button background paint
         btnPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         btnPaint.setColor(btnColor);
+
+        //Calculating button size
+        DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
+        btnSize = (int)((BTN_SIZE_DP * displayMetrics.density) + 0.5);
     }
 
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
-        int width = w - getPaddingLeft() - getPaddingRight();
-        int height = h - getPaddingTop() - getPaddingBottom();
+        int viewPadding = calculateViewPadding();
+        int btnStart = viewPadding;
+        int btnEnd = viewPadding + btnSize;
+        circleRect = new RectF(btnStart, btnStart, btnEnd, btnEnd);
 
-        int diameter = Math.min(width, height);
-        circleRect = new RectF(0, 0, diameter, diameter);
-        btnIcon.setBounds(0, 0, diameter, diameter);
+        int iconPadding = (int) (btnSize * 0.3);
+        int iconStart = btnStart + iconPadding;
+        int iconEnd = btnEnd - iconPadding;
+        btnIcon.setBounds(iconStart, iconStart, iconEnd, iconEnd);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int totalPadding = calculateViewPadding() * 2;
+        Log.v(TAG, "total padding = " + totalPadding);
+        setMeasuredDimension(btnSize + totalPadding, btnSize + totalPadding);
+        Log.v(TAG, "set dimension = " + (btnSize + totalPadding));
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         canvas.drawOval(circleRect, btnPaint);
         btnIcon.draw(canvas);
+    }
+
+    private int calculateViewPadding() {
+        return (int) (btnSize * 0.2);
     }
 
     public int getButtonColor() {
@@ -69,7 +94,6 @@ public class FloatingButton extends View {
     public void setButtonColorRes(int resId) {
         setButtonColor(getResources().getColor(resId));
     }
-
 
     public void setButtonColor(int btnColor) {
         this.btnColor = btnColor;
