620716e6696a1b6c034698893d2b06bd23e783bb
Suresh Subramaniam <sureshs.592@gmail.com>
2015-03-22 17:54:03 +0800

    Removing logs from the floating button

--- app/src/main/java/com/suresh/materialfun/ui/FloatingButton.java
+++ app/src/main/java/com/suresh/materialfun/ui/FloatingButton.java
@@ -9,7 +9,6 @@ import android.graphics.Paint;
 import android.graphics.RectF;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.View;
 
 import com.suresh.materialfun.R;
@@ -94,9 +93,7 @@ public class FloatingButton extends View {
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         int totalPadding = calculateViewPadding() * 2;
-        Log.v(TAG, "total padding = " + totalPadding);
         setMeasuredDimension(btnSize + totalPadding, btnSize + totalPadding);
-        Log.v(TAG, "set dimension = " + (btnSize + totalPadding));
     }
 
     @Override
